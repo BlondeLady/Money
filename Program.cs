ee42ca7a08bf39637252a0072a6bf6825b51b79b
BlondeLady <96891132+BlondeLady@users.noreply.github.com>
2022-01-06 23:39:47 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,22 +6,24 @@ using System.Threading.Tasks;
 
 namespace _1k2c_1._5_1_
 {
-    class Program
-    {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             Money mon1 = new Money();
             mon1.Grivna = 15;
             mon1.Kopiyka = 78;
             mon1[1] = 90;
-            Money mon2 = new Money(10, 23);
+            Money mon2 = new Money(10, 23, 0);
 
-            Money.DollarCost = 28.9;
+            Money total = mon1 + mon2;
+            Console.WriteLine($"Money mon1:{mon1}");
+            Console.WriteLine($"Money mon2:{mon2}");
+            Console.WriteLine($"Total money:{total}");
+
+            
             Console.WriteLine($"Total in dollar:{total.ToDollar()}");
             total.FromDollar(100);
             Console.WriteLine($"Total money:{total}");
-            Console.ReadKey()
+            Console.ReadKey();
 
         }
-    }
 }
